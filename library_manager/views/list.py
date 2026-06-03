fc23f878d04626a6a74662ded1979a340099ec3a
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-06-03 18:24:46 +0330

    Update show books and authors page
    View and template for showing books and authors are updated to show the
    data in a table format.

--- library_manager/views/list.py
+++ library_manager/views/list.py
@@ -1,6 +1,11 @@
+from typing import Any
+
 from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from django.views import View
+
+from library_manager.models.author import Author
+from library_manager.models.book import Book
 
 
 class ListView(View):
@@ -10,4 +15,26 @@ class ListView(View):
             # design a page that shows the error
             # provide a link to return home
 
-        return render(request, "library_manager/list.html", context={"entity": entity})
+        context: dict[str, Any] = {"entity": entity}
+
+        if entity == "book":
+            context["header"] = (
+                "Title",
+                "Published Date",
+                "ISBN",
+                "Authors",
+            )
+            print("*** book.objects", list(enumerate(Book.objects.all())))
+            context["data"] = enumerate(Book.objects.all())
+        elif entity == "author":
+            context["header"] = (
+                "First Name",
+                "Last Name",
+                "Age",
+                "Books",
+                "# of Books",
+            )
+            print("*** author.objects", list(enumerate(Author.objects.all())))
+            context["data"] = enumerate(Author.objects.all())
+
+        return render(request, "library_manager/list.html", context)
