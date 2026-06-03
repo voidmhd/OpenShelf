d0f4f89924da5365dac1bea53c7ca75d6317cf3b
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-06-03 17:36:17 +0330

    Change author model
    The number_of_published_books field has been commented. It will be
    calculated using database queries instead of being stored as a separate
    field.

--- library_manager/models/author.py
+++ library_manager/models/author.py
@@ -5,6 +5,6 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100, verbose_name="First Name")
     last_name = models.CharField(max_length=100, verbose_name="Last Name")
     age = models.IntegerField(verbose_name="Age")
-    number_of_published_books = models.IntegerField(
-        verbose_name="Number of Publications"
-    )
+    # number_of_published_books = models.IntegerField(
+    #     verbose_name="Number of Publications"
+    # )
