490e8d03433bf3948e30633195260f262b91f2f0
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-06-06 21:31:56 +0330

    Modify the add page
    The way to transfer data from the add view class to the django template
    has been modified to satisfy the abstraction rule (handling both book
    and author entity as an individual). Moreover, the template and the
    location of elements has been modified.

--- library_manager/views/add.py
+++ library_manager/views/add.py
@@ -1,7 +1,8 @@
 from typing import Any, Optional
 
 from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import redirect, render, resolve_url
+from django.urls import reverse
 from django.views import View
 
 from library_manager.models.author import Author
@@ -15,30 +16,36 @@ class AddView(View):
         context: dict[str, Any] = {"entity": entity}
 
         if entity == "book":
-            context["fields"] = [
-                ("id0", "text", "Title"),
-                ("id1", "date", "Published Date"),
-                ("id2", "text", "ISBN"),
-                ("id3", "list", "Authors"),
-            ]
-        elif entity == "author":
-            context["fields"] = [
-                Author.first_name,
-                Author.last_name,
-                Author.age,
-                Author.number_of_published_books,
+            context["inputs"] = [
+                ("text", "Title", "title"),
+                ("date", "Published Date", "published_date"),
+                ("text", "ISBN", "isbn"),
             ]
 
-        print("***", context)
+            context["list_values"] = Author.objects.all()
+
+        elif entity == "author":
+            context["inputs"] = [
+                ("text", "First Name", "first_name"),
+                ("text", "Last Name", "last_name"),
+                ("number", "Age", "age"),
+            ]
+
+            context["list_values"] = Book.objects.all()
 
         return render(request, "library_manager/add.html", context)
 
     def post(self, request: HttpRequest, entity: str) -> HttpResponse:
         AddView.check_entity(entity)
 
-        return render(request, "", context={"entity": entity})
+        print("***", "you are in post")
+        print("***", request.POST)
+
+        return redirect("lib_mngr:add", entity=entity)
 
     @staticmethod
-    def check_entity(entity: str) -> HttpResponseNotFound:
+    def check_entity(entity: str) -> HttpResponseNotFound | None:
         if entity not in ["book", "author"]:
             return HttpResponseNotFound()
+        else:
+            return None
